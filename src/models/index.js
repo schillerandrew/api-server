cd3aae35b1944e9f803b5efefeb1e894a48e66ac
schillerandrew <schiller.andrew@gmail.com>
2022-06-26 19:19:48 -0700

    fixing Heroku deployment

--- src/models/index.js
+++ src/models/index.js
@@ -9,7 +9,14 @@ const DATABASE_URL = process.env.NODE_ENV === 'test'
   ? 'sqlite::memory'
   : process.env.DATABASE_URL || 'postgres://localhost:5432/api-server';
 
-const sequelize = new Sequelize(DATABASE_URL);
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 const animalModel = animalSchema(sequelize, DataTypes);
 const personModel = personSchema(sequelize, DataTypes);
